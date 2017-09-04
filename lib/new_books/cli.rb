b41923a38976d4c73a15e6e87719c8f0d8cf0dd8
github username <github email address>
2017-09-04 16:57:53 -0400

    add menu  method and goodby method

--- lib/new_books/cli.rb
+++ lib/new_books/cli.rb
@@ -4,6 +4,8 @@ class NewBooks::CLI
   def call
     puts "The Newest books are:"
     list_books
+    menu
+    goodb
   end
 
   def list_books
@@ -14,6 +16,29 @@ class NewBooks::CLI
     DOC
   end
 
+  def menu
+    input = nil
+    while input != "exit"
+        puts "Enter the number of the book you would like more info on:"
+        input = gets.strip
+      case input
+      when "1"
+        puts "More info on book one"
+      when "2"
+        puts "More info on book 2"
+      end
+    end
+  end
+
+def goodby
+  puts "Thanks for checking for new books! Keep reading!!
+
+That's the thing with magic. You've got to know it's still here, all around us,
+or it just stays invisible for you.
+Don't forget - no one else sees the world the way you do,
+so no one else can tell the stories that you have to tell.
+I want to touch the heart of the world and make it smile. ~Charles DeLint"
+end
 
 
 
@@ -55,4 +80,5 @@ class NewBooks::CLI
 
 
 
+#class end
 end
